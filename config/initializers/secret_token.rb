42627adca60b53c280010411ead9ca8500da5260
Ludo van den Boom <ludo@cubicphuse.nl>
2011-09-07 12:05:23 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitstat::Application.config.secret_token = 'a1659cb12e7eb893abd4ac62b352b9cd74eeb1fd3b4a67092956dbcbdb67095bc9cfc3db2a65b40d771f0f8c37a1f2ee9a762c4a6dd14cab33c370f3c0d50731'
